64a36ecf757fb8a7783bbd86179f574e34906267
Ronald McIntyre <r.campbell.mcintyre@gmail.com>
2020-03-18 11:19:03 -0400

    mvp

---
+++ src/actions/actions.js
@@ -0,0 +1,8 @@
+
+export const buyItem = item => {
+    return { type: 'ADD_FEATURE', payload: item}
+  };
+
+export const removeItem = item => {
+    return { type: 'REMOVE_FEATURE', payload: item}
+};
